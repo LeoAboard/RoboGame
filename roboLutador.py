ff48abacedae190894482e57a37e0fa34f50786d
Leo <146775205+LeoAboard@users.noreply.github.com>
2025-02-11 17:35:03 -0400

    Update roboLutador.py

--- roboLutador.py
+++ roboLutador.py
@@ -3,15 +3,26 @@ from robo import robo
 
 class roboLutador(robo):
     def __init__(self, nome):
-        super().__init(nome)
+        super().__init__(nome)
         self.dano_max = 0.3
         self.poder = round(random.uniform(self.dano_max, 1), 2)
 
     def atacar(self, robo2:'robo') -> None:
-        robo2.vida = (1 - self.poder) * robo2.vida
-        print(f"{robo2.nome}: Ouch!")
 
-        if(isinstance(robo2, roboLutador)):
-            self.vida = (1 - robo2.poder) * self.vida
-            print(f"{self.nome}: Ouch!")
-        
+        if self.vida > 0.009:
+            robo2.vida = (1 - self.poder) * robo2.vida
+            print(f"{self.nome} ATACOU {robo2.nome}")
+            print(f"{robo2.nome}: Ouch!")
+            print(f"{robo2.vida:.2f}\n")
+
+            if(isinstance(robo2, roboLutador) and robo2.vida > 0.009):
+
+                self.vida = (1 - robo2.poder) * self.vida
+                print(f"{robo2.nome} ATACOU {self.nome}")
+                print(f"{self.nome}: Ouch!")
+                print(f"{self.vida:.2f}\n")
+            else:
+                print(f"{robo2.nome} está fraco e não pode atacar!\n")
+        else:
+            print(f"{self.nome} está fraco e não pode atacar!\n")
+            
