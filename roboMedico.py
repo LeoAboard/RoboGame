26742545bb523d4b7b02674211819905f73d6260
Leo <146775205+LeoAboard@users.noreply.github.com>
2025-02-11 17:35:20 -0400

    Update roboMedico.py

--- roboMedico.py
+++ roboMedico.py
@@ -6,8 +6,14 @@ class roboMedico(robo):
         super().__init__(nome)
         self.poder_de_cura = round(random.random(), 2)
 
-    def curar(self, robo2:'robo') -> None:
-        if self.vida >= robo2.vida:
+    def curar(self, robo2:'robo') -> bool:
+        chance = random.random()
+
+        if(robo2.precisa_de_medico) and (self.vida >= robo2.vida) and (chance > 0.5):
             robo2.vida = robo2.vida + self.poder_de_cura
+            print(f"{robo2.nome} foi curado!")
+            print(f"{robo2.vida:.2f}\n")
+            return True
         else:
-            print("A cura falhou...")
+            print("A cura falhou...\n")
+            return False
